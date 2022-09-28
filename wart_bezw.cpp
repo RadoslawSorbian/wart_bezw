eb4503b7b9202036ea002933bbca3295bc1aa007
Radosław Sorbian <Radoslaw.Sorbian@drzewniak.slupsk.pl>
2022-09-28 11:23:30 +0200

    Dodaj pliki projektów.

---
+++ wart_bezw.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x;						//deklaracja zmiennej typu double
+	cout << "Podaj libczbe, z ktorej chcesz obliczyc wartosc bezwzgledna: ";
+	cin >> x;						//wczytanie z klawaitury
+	cout << "|" << x << "| = ";
+	if (x >= 0)					//sprawdzenie czy liczba jest wieksza badz rowna zero
+		cout << x << endl;
+	else
+		cout << x * (-1) << endl;
+	return 0;
+}
+
